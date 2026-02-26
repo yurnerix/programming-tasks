34848876a872e3cfd8c8d6c794771bcffbdbcc4e
Yuriy <100475565+yurnerix@users.noreply.github.com>
2026-02-26 20:54:29 +0300

    Add files via upload

---
+++ Java/Cycles/Task3/Task3.java
@@ -0,0 +1,16 @@
+package Cycles;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int h = 0;
+        while (h < 5) {
+            int w = 0;
+            while (w < 10) {
+                System.out.print('Q');
+                w++;
+            }
+            System.out.println();
+            h++;
+        }
+    }
+}
