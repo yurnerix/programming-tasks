f7160ba5f3402c9a1bfefbdb0609a5ffeb87ecaa
Yuriy <100475565+yurnerix@users.noreply.github.com>
2026-02-26 20:43:54 +0300

    Add files via upload

---
+++ Java/Cycles/Task2/Task2.java
@@ -0,0 +1,15 @@
+package Cycles;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        for (int code = 1; code <= 10; code++) {
+            if (code == 5) {
+                System.out.println("Код найден! Замок открыт");
+                break;
+            }
+
+        }
+
+    }
+}
