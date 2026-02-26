9ce9b0a442244b1044515dd104bbc665404eb988
Yuriy <100475565+yurnerix@users.noreply.github.com>
2026-02-26 21:05:30 +0300

    Add files via upload

---
+++ Java/Cycles/Task7/Task7.java
@@ -0,0 +1,12 @@
+package Cycles;
+
+public class Task7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print('8');
+            }
+            System.out.println();
+        }
+    }
+}
