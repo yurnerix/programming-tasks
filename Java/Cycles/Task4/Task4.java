08a804185ed4126ff388605f79c18461c690f049
Yuriy <100475565+yurnerix@users.noreply.github.com>
2026-02-26 20:55:34 +0300

    Add files via upload

---
+++ Java/Cycles/Task4/Task4.java
@@ -0,0 +1,20 @@
+package Cycles;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int height = 1;
+        while (height <= 10) {
+            int width = 1;
+            while (width <= 20) {
+                if (height == 1 || height == 10 || width == 1 || width == 20) {
+                    System.out.print('Б');
+                } else {
+                    System.out.print(' ');
+                }
+                width++;
+            }
+            System.out.println();
+            height++;
+        }
+    }
+}
