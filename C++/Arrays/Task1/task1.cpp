4f1e2a978db911ec7c7cbe49bc1ecb558ef61dbf
Yuriy <100475565+yurnerix@users.noreply.github.com>
2026-03-02 09:56:08 +0300

    Add files via upload

---
+++ C++/Arrays/Task1/task1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    vector<double> profit(12);
+    for (double& p : profit) {
+        cin >> p;
+    }
+
+    int L, R;
+    cin >> L >> R;
+
+    L--;
+    R--;
+
+    int maxMonth = L;
+    int minMonth = L;
+
+    for (int i = L + 1; i <= R; ++i) {
+        if (profit[i] > profit[maxMonth]) maxMonth = i;
+        if (profit[i] < profit[minMonth]) minMonth = i;
+    }
+
+    cout << "Максимум в месяце: " << maxMonth + 1 << '\n';
+    cout << "Минимум в месяце: " << minMonth + 1 << '\n';
+
+    return 0;
+}
